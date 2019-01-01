5d6b6a27c064b89574b7b4faf509dd4b3f6581b8
Magnus Kruschwitz <magnuskruschwitz@magnus-macbook-pro.local>
2019-01-01 12:01:07 +0100

    Initial Commit

---
+++ Aufgabe_7_2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Aufgabe_7_2
+//
+//  Created by Magnus Kruschwitz on 01.01.19.
+//  Copyright © 2019 Magnus Kruschwitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
